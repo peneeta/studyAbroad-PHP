990d19e81ded32f92e49e03beb508320a5801b58
Yaman Shrestha <shresthayaman@hotmail.com>
2021-06-23 12:51:55 -0400

    added same comments to addToPlanner:

--- addToPlanner.php
+++ addToPlanner.php
@@ -1,7 +1,7 @@
 <?php 
 require 'connect.php';
 header('Access-Control-Allow-Origin: https://engineersabroad.uvacreate.virginia.edu'); //comment out when using localhost
-//header('Access-Control-Allow-Origin: http://localhost:4200');
+//header('Access-Control-Allow-Origin: http://localhost:4200'); //comment out when using hosted server
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
 header('Access-Control-Max-Age: 1000');  
